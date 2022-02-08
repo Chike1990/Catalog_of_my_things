e3ca1f494abbea6286eaba74808e206a2201f500
Chike1990 <chibuikeuzoechina@gmail.com>
2022-02-08 23:25:26 +0100

    Add a method to game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,16 +1,20 @@
-require "./item"
+require './item'
+require 'date'
 
 class Game < Item
-  attr_accessor :last_played_at, :multiplayer 
+  attr_accessor :last_played_at, :multiplayer
+  attr_reader :publish_date
 
-  def initialize(multiplayer, last_played_at)
-    super()
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   private
 
-  def can_be_achived?()
+  def can_be_achived?(date)
+    true if super.publish_date && @last_played_at > 2
+    false
   end
 end
