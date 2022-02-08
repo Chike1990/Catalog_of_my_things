bc60128ee2909ddec4c241739f297a851dada0cf
micheaol <micheaol80@gmail.com>
2022-02-08 21:20:13 +0100

    Add List album class

--- classes/list_music_album.rb
+++ classes/list_music_album.rb
@@ -1,5 +1,7 @@
-class ClassName
+class ListAlbum
     def self.list_album(albums)
-        
+        puts 'Your library is empty' if albums.empty?
+        albums.each_with_index do |album, index| puts album end
     end
 end
+
