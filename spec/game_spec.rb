a49025b1f1b323f1724e4977c0f0544a66279b0b
Chike1990 <chibuikeuzoechina@gmail.com>
2022-02-09 21:29:12 +0100

    Fixes issues

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,36 +2,37 @@ require_relative '../classes/game'
 
 describe 'test Game class' do
   before :each do
-    @game = %w(Game.new(true, '2020-01-01', '2005-01-01'), Game.new(true, '2022-01-01', '2022-02-02'))
+    @game = [Game.new(true, 2020 - 0o1 - 0o1, 2005 - 0o1 - 0o1), Game.new(true, 2022 - 0o1 - 0o1, 2022 - 0o2 - 0o2)]
   end
 
-describe 'test Label class' do
-  before(:each) do
-    @label = Label.new('books', 'Red')
-  end
-
-  it 'instantiates a Game' do
-    @game.each do |game|
-      expect(game).to be_instance_of Game
+  describe 'test Label class' do
+    before(:each) do
+      @label = Label.new('books', 'Red')
     end
-  end
 
-  it 'instantiates a Game' do
-    expect(@game.each).to be_instance_of Game
-  end
-
-  it 'checks if game is kind of Item' do
-    @game.each do |game|
-      expect(game).to be_kind_of Item
+    it 'instantiates a Game' do
+      @game.each do |game|
+        expect(game).to be_instance_of Game
+      end
     end
-  end
 
-  it 'can be achieved' do
-    expect(@game[0].can_be_archived?).to be(true)
-  end
+    it 'instantiates a Game' do
+      expect(@game.each).to be_instance_of Game
+    end
 
-  it 'cannot be achieved' do
-    expect(@game[1].can_be_archived?).to be(false)
+    it 'checks if game is kind of Item' do
+      @game.each do |game|
+        expect(game).to be_kind_of Item
+      end
+    end
+
+    it 'can be achieved' do
+      expect(@game[0].can_be_archived?).to be(true)
+    end
+
+    it 'cannot be achieved' do
+      expect(@game[1].can_be_archived?).to be(false)
+    end
   end
 end
 
